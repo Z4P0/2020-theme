6a6000bd8d62e126fed2804e8bb550a1103b6461
luis rosario <lrosario101@gmail.com>
2020-05-23 17:08:20 -0400

    highlight current location in nav

--- scripts/domcontentloaded.js
+++ scripts/domcontentloaded.js
@@ -1,7 +1,29 @@
+// ----------------------------------------
 document.querySelector('html').classList.toggle('no-js');
 
+// ----------------------------------------
 let sourceSans = new FontFaceObserver('Source Sans Pro');
 
 sourceSans.load().then(() => {
   document.querySelector('body').classList.add('font-loaded');
 });
+
+
+// ----------------------------------------
+// highlight the nav item
+// ----------------------------------------
+const locationURL = location.origin + location.pathname;
+
+const navLinks = document.querySelectorAll('.site-nav-list a');
+
+
+for (var i = 0; i < navLinks.length; i++) {
+
+  let anchor = navLinks[i];
+
+  if (anchor.href == locationURL) {
+    anchor.parentElement.classList.toggle('site-nav-list--active');
+    break;
+  }
+
+}
